328c4d8b8db92f15756cff04bb8b9724480e9ae2
avicndugu <avicnduguh@gmail.com>
2019-06-25 16:03:48 +0300

    All background colors switching works

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ const logo =document.getElementById('logo');
 const navigation=document.getElementById('navigation');
 const verticalText=document.querySelector('.vertical-text');
 const switchPin= document.querySelector('#switch-pin');
-const slogan = document.querySelector('.slogan');
+const section2=document.getElementById('section-2');
+const next= document.getElementById('next');
+const collection=document.getElementById('collection');
+// const slogan = document.querySelector('.slogan');
 document.querySelector('#switch').addEventListener('click',function(){
     if (this.style.alignItems=="" || this.style.alignItems=="flex-start"){
         this.style.alignItems="flex-end";
@@ -25,9 +28,12 @@ document.querySelector('#switch').addEventListener('click',function(){
 
 function darkTheme() {
     logo.style.backgroundColor="#55575b";
-    navigation.style.backgroundColor="#2f3136";
+    navigation.style.backgroundColor="#2d2f34";
     verticalText.style.background="#55575b";
-    slogan.style.background="#36393f";
+    section2.style.background="#55575b";
+    next.style.background="#fff";
+    collection.style.background="#2d2f34"
+    // slogan.style.background="#36393f";
 
 }
 
@@ -35,5 +41,8 @@ function lightTheme() {
     logo.style.backgroundColor="#000";
     navigation.style.backgroundColor="#fff";
     verticalText.style.background="#000";
-    slogan.style.background='#eac7ff';
+    section2.style.background="#eac7ff";
+    next.style.background="#cdcdcd";
+    collection.style.background="#111";
+    // slogan.style.background='#eac7ff';
 }
